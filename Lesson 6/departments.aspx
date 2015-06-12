52bc7ae2287419107245829037fcd9d5ee4930c0
Patrick Richey <patrick_richey@hotmail.com>
2015-06-11 23:07:26 -0400

    I am become master

--- Lesson 6/departments.aspx
+++ Lesson 6/departments.aspx
@@ -2,4 +2,17 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Departments</h1>
+    <a href="department.aspx">Add Departments</a>
+    <asp:GridView ID="grdDepartments" runat="server" CssClass="table table-striped table-hover" AutoGenerateColumns="false" 
+        OnRowDeleting="grdDepartments_RowDeleting" DataKeyNames="DepartmentID">
+        <Columns>
+            <asp:BoundField DataField="DepartmentID" HeaderText="Department ID" />
+            <asp:BoundField DataField="Name" HeaderText="Department Name" />
+            <asp:BoundField DataField="Budget" HeaderText="Budget" DataFormatString="{0:C}"/>
+            <asp:HyperLinkField HeaderText="Edit" Text="Edit" NavigateUrl="department.aspx" 
+                DataNavigateUrlFields="DepartmentID" DataNavigateUrlFormatString="department.aspx?DepartmentID={0}"/>
+            <asp:CommandField HeaderText="Delete" DeleteText="Delete" ShowDeleteButton="true"/>
+        </Columns>
+    </asp:GridView>
 </asp:Content>
--- Lesson 6/departments.aspx.cs
+++ Lesson 6/departments.aspx.cs
@@ -5,13 +5,61 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//reference the EF Models
+using Lesson_6.Models;
+using System.Web.ModelBinding;
+
 namespace Lesson_6
 {
     public partial class departments : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //if loading the page for the first time, populate the student grid
+            if (!IsPostBack)
+            {
+                GetDepartments();
+            }
+        }
 
+
+        protected void GetDepartments()
+        {
+            //connect to EF
+            using (comp2007Entities db = new comp2007Entities())
+            {
+                //query the students table using EF and LINQ
+                var Departments = from s in db.Departments
+                               select s;
+
+                //bind the result to the gridview
+                grdDepartments.DataSource = Departments.ToList();
+                grdDepartments.DataBind();
+            }
+        }
+
+        protected void grdDepartments_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            //store which row was clicked
+            Int32 selectedRow = e.RowIndex;
+
+            //get the selected StudentID using the grid's Data Key collection
+            Int32 DepartmentID = Convert.ToInt32(grdDepartments.DataKeys[selectedRow].Values["DepartmentID"]);
+
+            //use EF to remove the selected student from the db
+            using (comp2007Entities db = new comp2007Entities())
+            {
+                Department s = (from objs in db.Departments
+                             where objs.DepartmentID == DepartmentID
+                             select objs).FirstOrDefault();
+
+                //do the delete
+                db.Departments.Remove(s);
+                db.SaveChanges();
+            }
+
+            //refresh the grid
+            GetDepartments();
         }
     }
 }
